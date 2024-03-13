d5f2b98ccac665ff6da5e80514c6d2ef3e66f564
rachel <413843464@qq.com>
2024-03-13 13:32:27 +0800

    整合elasticsearch 测试相关api

--- gulimall-search/src/main/java/com/rachel/hulimall/gulimallsearch/config/GulimallElasticsearchConfig.java
+++ gulimall-search/src/main/java/com/rachel/hulimall/gulimallsearch/config/GulimallElasticsearchConfig.java
@@ -1,19 +1,28 @@
-//package com.rachel.hulimall.gulimallsearch.config;
-//
-//import org.apache.http.HttpHost;
-//import org.elasticsearch.client.RestClient;
-//import org.elasticsearch.client.RestClientBuilder;
-//import org.elasticsearch.client.RestHighLevelClient;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class GulimallElasticsearchConfig {
-//
-//    @Bean
-//    public RestHighLevelClient esClient() {
-//        RestClientBuilder builder = RestClient.builder(new HttpHost("127.0.0.1", 9200, "http"));
-//        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
-//        return restHighLevelClient;
-//    }
-//}
+package com.rachel.hulimall.gulimallsearch.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GulimallElasticsearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esClient() {
+        RestClientBuilder builder = RestClient.builder(new HttpHost("192.168.56.10", 9200, "http"));
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
+        return restHighLevelClient;
+    }
+
+}
--- gulimall-search/src/test/java/com/rachel/hulimall/gulimallsearch/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/rachel/hulimall/gulimallsearch/GulimallSearchApplicationTests.java
@@ -1,27 +1,48 @@
 package com.rachel.hulimall.gulimallsearch;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
+import com.alibaba.fastjson.JSON;
+import com.rachel.hulimall.gulimallsearch.config.GulimallElasticsearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContent;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
+
 @SpringBootTest
 class GulimallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient esClient;
+
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("user");
+        indexRequest.id("1");
+        User user = new User();
+        user.setName("rachel");
+        user.setAge(25);
+        indexRequest.source(JSON.toJSON(user), XContentType.JSON);
+        IndexResponse indexResponse = esClient.index(indexRequest, GulimallElasticsearchConfig.COMMON_OPTIONS);
+        System.out.println(indexResponse);
+    }
+
+    @Data
+    class User {
+        private String name;
+        private Integer age;
+    }
 
 
     @Test
     public void contextLoads() {
-
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200, "http"),
-                new HttpHost("localhost", 9201, "http")).build();
-
+        System.out.println(esClient);
 
     }
 
