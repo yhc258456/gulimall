1aaee5a8d81afba806311f206e80edb00fa68a5d
rachel <413843464@qq.com>
2024-03-18 19:54:55 +0800

    基于es实现商品的查询、聚合、高亮

---
+++ gulimall-search/src/main/java/com/rachel/hulimall/gulimallsearch/controller/SearchController.java
@@ -0,0 +1,35 @@
+package com.rachel.hulimall.gulimallsearch.controller;
+
+import com.rachel.hulimall.gulimallsearch.service.MallSearchService;
+import com.rachel.hulimall.gulimallsearch.vo.SearchParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SearchController {
+
+
+    @Autowired
+    private MallSearchService mallSearchService;
+
+    @GetMapping("/list.html")
+    public String list(SearchParam searchParam, Model model, HttpServletRequest request) {
+        searchParam.set_queryString(request.getQueryString());
+        Object result = mallSearchService.search(searchParam);
+        model.addAttribute("result", result);
+        return "list";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String list() {
+
+        return "Hello World";
+    }
+
+}
---
+++ gulimall-search/src/main/java/com/rachel/hulimall/gulimallsearch/service/MallSearchService.java
@@ -0,0 +1,9 @@
+package com.rachel.hulimall.gulimallsearch.service;
+
+import com.rachel.hulimall.gulimallsearch.vo.SearchParam;
+
+public interface MallSearchService {
+
+    Object search(SearchParam searchParam);
+
+}
